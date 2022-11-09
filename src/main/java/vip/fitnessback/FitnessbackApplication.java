0acf082894a6cace82eda18dca47db8379a828c3
Yameogo123 <58187516+Yameogo123@users.noreply.github.com>
2022-11-09 23:09:29 +0100

    Update FitnessbackApplication.java

--- src/main/java/vip/fitnessback/FitnessbackApplication.java
+++ src/main/java/vip/fitnessback/FitnessbackApplication.java
@@ -5,9 +5,13 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import vip.fitnessback.repository.UserRepository;
+import vip.fitnessback.model.User;
 
 @SpringBootApplication
 public class FitnessbackApplication implements CommandLineRunner {
+	
+	@Autowired
+	private UserRepository userRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(FitnessbackApplication.class, args);
@@ -15,6 +19,9 @@ public class FitnessbackApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-
+		User user= new User();
+		user.setPassword("$2a$10$6aGSyRfjkzYI4yR6MqZxbedPycQry0RMEkqGVqj0ClP8/.iBoe5k6");
+		user.setUsername("admin@gmail.com");
+		userRepository.save(user);
 	}
 }
